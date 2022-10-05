5899ea3da0b4b327d58ea617329a13e32e463b1c
Shankar Maes <shankar.maes@student.howest.be>
2022-10-05 17:44:05 +0200

    commit

--- script/app.js
+++ script/app.js
@@ -1,13 +1,21 @@
-function handleFloatingLabel() {
-   
-}
+function handleFloatingLabel() {}
+
 
 function handlePasswordSwitcher() {
-    
+  console.log('handlePasswordSwitcher');
+  const passwordInput = document.querySelector('.c-input');
+  const passwordToggle = document.querySelector('.c-toggle-password__checkbox');
+  passwordToggle.addEventListener('change', function () {
+    if (passwordToggle.checked) {
+      passwordInput.type = 'text';
+    } else {
+      passwordInput.type = 'password';
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-    handlePasswordSwitcher();
+  console.log('Script loaded!');
+  handleFloatingLabel();
+  handlePasswordSwitcher();
 });
