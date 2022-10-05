1224f512565f87ef6038e6a6f9446dd3362162fd
Shankar Maes <shankar.maes@student.howest.be>
2022-10-05 18:57:32 +0200

    commit

--- script/app.js
+++ script/app.js
@@ -1,21 +1,40 @@
-function handleFloatingLabel() {}
+let email = {},
+password = {},
+signInButton;
 
 
+function handleFloatingLabel() {
+    console.log('handleFloatingLabel');
+    const label = document.querySelectorAll('.c-label_float');
+    const input_float= document.querySelectorAll('.c-input');
+    input_float.addEventListener('focus', function (){
+        if(this.value.length > 0){
+        label.classList.add('is-floating');
+        }
+    });
+}
+
+
+const passwordOptions = ['password', 'text'];
 function handlePasswordSwitcher() {
   console.log('handlePasswordSwitcher');
-  const passwordInput = document.querySelector('.c-input');
+  const passwordInput = document.querySelector('.c-input_password');
   const passwordToggle = document.querySelector('.c-toggle-password__checkbox');
   passwordToggle.addEventListener('change', function () {
-    if (passwordToggle.checked) {
-      passwordInput.type = 'text';
-    } else {
-      passwordInput.type = 'password';
-    }
+    passwordInput.type = passwordOptions[+this.checked];
   });
+
+}
+
+function getDOMElements(){
+    email = document.querySelector('.js-input_email');
+    password = document.querySelector('.js-input_password');
+    signInButton = document.querySelector('.js-sign-in-button');    
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('Script loaded!');
   handleFloatingLabel();
   handlePasswordSwitcher();
+  getDOMElements();
 });
